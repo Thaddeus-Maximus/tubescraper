84c4dd29f24ddedc2cde419bdefa6610607dc3d9
Thaddeus Hughes <hughes.thad@gmail.com>
2025-01-28 08:49:43 -0600

    verbose output, add user agent

--- downloader.py
+++ downloader.py
@@ -141,8 +141,9 @@ def download(pkg):
             "outtmpl": library_location+'%(id)s',
             "quiet": False,
             "no_warnings": False,
-            "nooverwrites": False
-            #"user_agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0"
+            "nooverwrites": False,
+            "verbose": True,
+            "user_agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0"
         }
 
 
